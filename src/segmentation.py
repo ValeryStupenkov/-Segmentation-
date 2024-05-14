ac93b1e2550637bd36294ac460d66a56d72a45bf
Валерий Ступеньков <stupenkov.val@yandex.ru>
2024-05-14 20:51:33 +0300

    Add final segmentation model

--- src/segmentation.py
+++ src/segmentation.py
@@ -2,6 +2,9 @@ from PIL import Image
 import torchvision
 import warnings
 import torch
+import random
+import numpy as np
+import cv2
 
 def categories_from_txt(path):
     categories = []
@@ -10,6 +13,7 @@ def categories_from_txt(path):
             categories.append(line.strip())
 
     return categories
+
 
 def filter(output):
     boxes = []
@@ -24,8 +28,30 @@ def filter(output):
 
     return boxes, labels, scores
 
+
+def segmented_image(img, boxes, labels, scores):
+    categories = categories_from_txt('../data/categories.txt')
+
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in categories]
+
+    result_image = np.array(img)
+
+    for box, label, score, in zip(boxes, labels, scores):
+        color = random.choice(colors)
+        tl = round(0.002 * max(result_image.shape[0:2])) + 1
+        c1, c2 = (box[0], box[1]), (box[2], box[3])
+        cv2.rectangle(result_image, c1, c2, color, thickness=tl)
+        display_txt = "%s: %.1f%%" % (categories[label], 100*score)
+        tf = max(tl - 1, 1)
+        t_size = cv2.getTextSize(display_txt, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(result_image, c1, c2, color, -1)
+        cv2.putText(result_image, display_txt, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+    return Image.fromarray(result_image.astype(np.uint8))
+
+
 def segmentation(path):
-    path = 'St5FF-zqLHE.jpg'
     img = Image.open(path)
 
     torch.set_grad_enabled(False)
@@ -38,4 +64,4 @@ def segmentation(path):
 
     boxes, labels, scores = filter(output)
 
-    categories = categories_from_txt('../data/categories.txt')
+    return segmented_image(img, boxes, labels, scores)
