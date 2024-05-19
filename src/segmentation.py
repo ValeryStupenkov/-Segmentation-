e9ab2c77fe988c9aae9d758f0b915b4f1f1e86f7
Валерий Ступеньков <stupenkov.val@yandex.ru>
2024-05-19 12:17:02 +0300

    Add annotations to segmentation

--- src/segmentation.py
+++ src/segmentation.py
@@ -16,7 +16,7 @@ def torch2numpy(torch_dict: dict) -> dict:
 
     return np_dict
 
-def categories_from_txt(path):
+def categories_from_txt(path: str) -> list:
     """Read categories from a file and return array."""
     categories = []
     with open(path, 'r') as file:
@@ -26,7 +26,7 @@ def categories_from_txt(path):
     return categories
 
 
-def filter(output):
+def filter(output: dict) -> list | list | list:
     """Return objects with a probability >= 0.5."""
     boxes = []
     labels = []
@@ -56,7 +56,10 @@ def model_predictions(img: Image.Image) -> dict:
 
     return output
 
-def segmented_image(img, boxes, labels, scores):
+def segmented_image(img: np.ndarray,
+                    boxes: list,
+                    labels: list,
+                    scores: list) -> Image.Image:
     """Return a segmented image."""
     path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
                         "data", "categories.txt")
@@ -64,26 +67,24 @@ def segmented_image(img, boxes, labels, scores):
     
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in categories]
 
-    result_image = np.array(img)
-
     for box, label, score, in zip(boxes, labels, scores):
         color = random.choice(colors)
-        tl = round(0.002 * max(result_image.shape[0:2])) + 1
+        tl = round(0.002 * max(img.shape[0:2])) + 1
         c1, c2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
-        cv2.rectangle(result_image, c1, c2, color, thickness=tl)
+        cv2.rectangle(img, c1, c2, color, thickness=tl)
         text = "%s: %.1f%%" % (categories[label], 100*score)
         tf = max(tl - 1, 1)
         t_size = cv2.getTextSize(text, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(result_image, c1, c2, color, -1)
-        cv2.putText(result_image, text, 
+        cv2.rectangle(img, c1, c2, color, -1)
+        cv2.putText(img, text, 
                     (c1[0], c1[1] - 2), 0, tl / 3, 
                     [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
-    return Image.fromarray(result_image.astype(np.uint8))
+    return Image.fromarray(img.astype(np.uint8))
 
 
-def segmentation(path):
+def segmentation(path: str) -> Image.Image:
     """Segment an image."""
     img = Image.open(path)
 
@@ -98,4 +99,4 @@ def segmentation(path):
 
     boxes, labels, scores = filter(output)
 
-    return segmented_image(img, boxes, labels, scores)
+    return segmented_image(np.array(img), boxes, labels, scores)
