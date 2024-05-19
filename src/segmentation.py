de5aeea81e508bbb7364202d7c66ca19def93f0c
Тихонов Максим <graycubeu@gmail.com>
2024-05-19 11:22:24 +0300

    numpy output in segmentation

--- src/segmentation.py
+++ src/segmentation.py
@@ -6,6 +6,15 @@ import random
 import numpy as np
 import cv2
 
+def torch2numpy(torch_dict: dict) -> dict:
+    """Return a numpy dict from a torch dict."""
+    np_dict = {}
+
+    for k in torch_dict.keys():
+        np_dict[k] = torch_dict[k].cpu().numpy()
+
+    return np_dict
+
 def categories_from_txt(path):
     categories = []
     with open(path, 'r') as file:
@@ -22,12 +31,27 @@ def filter(output):
 
     for i in range(len(output['scores'])):
         if output['scores'][i] >= 0.5:
-            boxes.append(output['boxes'][i].int().cpu().numpy())
-            labels.append(output['labels'][i].cpu().numpy())
-            scores.append(output['scores'][i].cpu().numpy())
+            boxes.append(output['boxes'][i])
+            labels.append(output['labels'][i])
+            scores.append(output['scores'][i])
 
     return boxes, labels, scores
 
+def model_predictions(img: Image.Image) -> dict:
+    """Return pretrained model predictions."""
+    torch.set_grad_enabled(False)
+    warnings.filterwarnings("ignore")
+    pretrained_model = torchvision.models.detection.maskrcnn_resnet50_fpn(
+                       pretrained=True
+    )
+    pretrained_model = pretrained_model.eval().cpu()
+
+    img_tensor = torchvision.transforms.functional.to_tensor(img).cpu()
+
+    output = pretrained_model([img_tensor])[0]
+    output = torch2numpy(output)
+
+    return output
 
 def segmented_image(img, boxes, labels, scores):
     categories = categories_from_txt('../data/categories.txt')
@@ -39,7 +63,7 @@ def segmented_image(img, boxes, labels, scores):
     for box, label, score, in zip(boxes, labels, scores):
         color = random.choice(colors)
         tl = round(0.002 * max(result_image.shape[0:2])) + 1
-        c1, c2 = (box[0], box[1]), (box[2], box[3])
+        c1, c2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
         cv2.rectangle(result_image, c1, c2, color, thickness=tl)
         display_txt = "%s: %.1f%%" % (categories[label], 100*score)
         tf = max(tl - 1, 1)
@@ -54,13 +78,14 @@ def segmented_image(img, boxes, labels, scores):
 def segmentation(path):
     img = Image.open(path)
 
-    torch.set_grad_enabled(False)
-    warnings.filterwarnings("ignore")
-    pretrained_model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-    pretrained_model = pretrained_model.eval().cpu()
+    output = model_predictions(img)
+    #torch.set_grad_enabled(False)
+    #warnings.filterwarnings("ignore")
+    #pretrained_model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+    #pretrained_model = pretrained_model.eval().cpu()
 
-    img_tensor = torchvision.transforms.functional.to_tensor(img).cpu()
-    output = pretrained_model([img_tensor])[0]
+    #img_tensor = torchvision.transforms.functional.to_tensor(img).cpu()
+    #output = pretrained_model([img_tensor])[0]
 
     boxes, labels, scores = filter(output)
 
