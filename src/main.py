2b5cc304ee8eb433a4008c0d62da0a1129520326
ziy07@list.ru <ziy07@list.ru>
2024-05-19 22:15:33 +0300

    Update main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-from tkinter import *
+"""The main file that runs the application."""
+
+import tkinter as tk
 from tkinter import filedialog
 from PIL import ImageTk, Image
-import os
+from segmentation import segmentation
 
 
-def set_screen_shape(root, width, height):
+def set_screen_shape(root: tk.Tk, width: int, height: int) -> None:
+    """Set the screen size."""
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     total_width = (screen_width // 2) - (width // 2)
@@ -12,11 +15,12 @@ def set_screen_shape(root, width, height):
     root.geometry(f'{width}x{height}+{total_width}+{total_height}')
 
 
-def image_upload():
-    home_dir = os.path.expanduser('~')
-    path = filedialog.askopenfilename(initialdir=os.path.join(home_dir, 'Pictures'),
-                                      filetypes=[('Image File','.jpg .png .jpeg')])
-    
+def image_upload() -> None:
+    """Load an image and display a segmented image on the screen."""
+    path = filedialog.askopenfilename(
+           filetypes=[('Image File', '.jpg .png .jpeg')]
+    )
+
     if not len(path):
         return
 
@@ -24,21 +28,29 @@ def image_upload():
     upload_bttn.destroy()
 
     raw_img = ImageTk.PhotoImage(img)
-    img_show = Label(root, image=raw_img)
+    img_show = tk.Label(root, image=raw_img)
     img_show.image = raw_img
 
     set_screen_shape(root, img.width, img.height)
-
     img_show.pack(side='bottom', fill='both', expand='yes')
+
+    segmented_img = segmentation(path)
+
+    segmented_img = ImageTk.PhotoImage(segmented_img)
+    segmented_img_show = tk.Label(root, image=segmented_img)
+    segmented_img_show.image = segmented_img
+    img_show.destroy()
+    segmented_img_show.pack(side='bottom', fill='both', expand='yes')
 
 
 if __name__ == '__main__':
-    root = Tk()
+    root = tk.Tk()
+
     root.title('Segmentation')
 
     set_screen_shape(root, 500, 300)
 
-    upload_bttn = Button(root, text='Choose image', command=image_upload)
+    upload_bttn = tk.Button(root, text='Choose image', command=image_upload)
     upload_bttn.pack(side='bottom', pady=50)
 
     root.mainloop()
